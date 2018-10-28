9666ad8d319279d89d18b900d1932aa7d8460e04
Vong <wangle@meitu.com>
2018-10-28 22:04:10 +0800

    add custom commit message

---
+++ commit-message.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+
+# get clone master
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+
+cd ../
+mv .deploy_git/.git/ ./public/
+
+cd ./public
+
+git config user.name "Vong"
+git config user.email "Vong9262@gmail.com"
+
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Update at `date +"%Y-%m-%d %H:%M"`"
+
+# Github Pages
+git push --force --quiet "https://${BlogCI}@${GH_REF}" master:master

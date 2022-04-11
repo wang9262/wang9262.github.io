409eab2c814afa90c2f212e7b5f4f0f144d14fcf
Vong <Vong9262@gmail.com>
2022-04-11 18:50:31 +0800

    Publish deploy 2022-04-11 18:50

---
+++ Script/baidu_analyse.js
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vong on 2022/4/10.
+//
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8aa6c37a6671fca3a595bb5c714cafc7";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ Script/google_analyse.js
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vong on 2022/4/10.
+//
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-ZHSZ71NFYK');

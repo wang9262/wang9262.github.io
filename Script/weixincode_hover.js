0cf40c4d6d215a302a1b033e81ccd339400c5bf3
Vong <Vong9262@gmail.com>
2022-04-14 18:59:10 +0800

    Publish deploy 2022-04-14 18:59

--- Script/weixincode_hover.js
+++ Script/weixincode_hover.js
@@ -6,7 +6,12 @@
 //
 var weixinHeadButton = $('.headIconWeixin');
 weixinHeadButton.hover(
+    // in
     function(){
         $('.weixinHeadQcode').css('display','block');
+    },
+    // out
+    function(){
+        $('.weixinHeadQcode').css('display','none');
     }
 )

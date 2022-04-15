f813525ea14070e5136f2f5d1185a694568cadd6
Vong <Vong9262@gmail.com>
2022-04-15 22:49:26 +0800

    Publish deploy 2022-04-15 22:49

--- Script/baidu_analyse.js
+++ Script/baidu_analyse.js
@@ -8,7 +8,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8aa6c37a6671fca3a595bb5c714cafc7";
+  hm.src = "https://hm.baidu.com/hm.js?67c5066977a32b89fe5131c2d0849e8c";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
